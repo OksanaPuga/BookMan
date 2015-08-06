56c775beebcbb9385d08adf0bcb6cb06d490bbeb
AndreyTrotc <andrey.trotc@gmail.com>
2015-08-06 23:20:18 +0300

    Added responsive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('js works');
+$(document).ready(function () {
+    var $asidelink = $('.aside-link'), $wrapsidebar = $('.wrap-sidebar'), $btnmenu = $('.btn-menu');
+    $asidelink.click(function () {
+        $asidelink.toggleClass('active');
+        $wrapsidebar.toggleClass('active');
+        return false;
+    });
+    $btnmenu.click(function () {
+        $btnmenu.toggleClass('active');
+        $btnmenu.next().toggleClass('active');
+        return false;
+    });
+
+});
+    
