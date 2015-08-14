af03ff163229eaffb4c9130b0e6404ae0dae9c91
Oksana_Puga <puga.oksana@gmail.com>
2015-08-14 13:44:30 +0300

    Minor changes in angular services
    changed saving to local storage and logic of inicialisation function.
    Still need to be fixed bug in first loading

--- dev/scripts/services/book-factory.js
+++ dev/scripts/services/book-factory.js
@@ -28,25 +28,25 @@ angular.module('bookman').factory('Book', function BookFactory() {
     };
     var currentBooks = [];
     BookFactory.prototype.save = function () {
-        if (sessionStorage.getItem('books')) {
-            currentBooks = angular.fromJson(sessionStorage.getItem('books'));
+        if (localStorage.getItem('books')) {
+            currentBooks = angular.fromJson(localStorage.getItem('books'));
         }
         currentBooks.push(this);
-        sessionStorage.setItem('books', angular.toJson(currentBooks));
+        localStorage.setItem('books', angular.toJson(currentBooks));
     };
     BookFactory.prototype.update = function () {
         var that = this,
             isNew = true;
-        if (!sessionStorage.getItem('books')) {
+        if (!localStorage.getItem('books')) {
             that.save();
         } else {
-            currentBooks = angular.fromJson(sessionStorage.getItem('books'));
+            currentBooks = angular.fromJson(localStorage.getItem('books'));
             angular.forEach(currentBooks, function (obj, index) {
                 if (obj.id == that.id) {
                     angular.forEach(that, function (value, property) {
                         obj[property] = value;
                     });
-                    sessionStorage.setItem('books', angular.toJson(currentBooks));
+                    localStorage.setItem('books', angular.toJson(currentBooks));
                     isNew = false;
                 }
             });
--- dev/scripts/services/books-factory.js
+++ dev/scripts/services/books-factory.js
@@ -2,13 +2,41 @@ angular.module('bookman').factory('Books', function BooksFactory($http, Book) {
     var factory = {};
 
     factory.init = function () {
-        return $http.get("https://www.googleapis.com/books/v1/users/114873229240336350134/bookshelves/0/volumes");
+        var rawBooksArray = $http.get("https://www.googleapis.com/books/v1/users/114873229240336350134/bookshelves/0/volumes");
+        rawBooksArray.success(function (response) {
+            var book, elm, obj, appBooks = response.items;
+            for (var property in appBooks) {
+                elm = appBooks[property];
+                obj = {
+                    id: elm.id,
+                    title: elm.volumeInfo.title,
+                    author: elm.volumeInfo.authors,
+                    publisher: elm.volumeInfo.publisher,
+                    publicationDate: elm.volumeInfo.publishedDate,
+                    image: elm.volumeInfo.imageLinks.thumbnail,
+                    category: elm.volumeInfo.categories,
+                    length: elm.volumeInfo.pageCount,
+                    description: elm.volumeInfo.description
+                }
+                book = new Book(obj);
+                book.update();
+            }
+        });
     };
+
+
+    (function () {
+        if (!localStorage.getItem('books')) {
+            factory.init();
+        }
+    })();
+
+
     factory.getAllBooks = function () {
-        return angular.fromJson(sessionStorage.getItem('books'));
+        return angular.fromJson(localStorage.getItem('books'));
     };
     factory.getBookByID = function (id) {
-        var books = angular.fromJson(sessionStorage.getItem('books'));
+        var books = angular.fromJson(localStorage.getItem('books'));
         for (var i = 0; i < books.length; i++) {
             if (books[i].id === id) {
                 return books[i];
@@ -16,24 +44,5 @@ angular.module('bookman').factory('Books', function BooksFactory($http, Book) {
         }
     };
 
-    factory.init().success(function (response) {
-        var book, elm, obj, appBooks = response.items;
-        for (var property in appBooks) {
-            elm = appBooks[property];
-            obj = {
-                id: elm.id,
-                title: elm.volumeInfo.title,
-                author: elm.volumeInfo.authors,
-                publisher: elm.volumeInfo.publisher,
-                publicationDate: elm.volumeInfo.publishedDate,
-                image: elm.volumeInfo.imageLinks.thumbnail,
-                category: elm.volumeInfo.categories,
-                length: elm.volumeInfo.pageCount,
-                description: elm.volumeInfo.description
-            }
-            book = new Book(obj);
-            book.update();
-        }
-    });
     return factory;
 });
