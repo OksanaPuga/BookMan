7c89436139e9dca3bb732e17c07c4d28b2479138
Oksana_Puga <puga.oksana@gmail.com>
2015-08-16 02:03:09 +0300

    Added quick-statistics directive

---
+++ dev/scripts/directives/quick-statistics-directive.js
@@ -0,0 +1,9 @@
+angular.module('bookman')
+    .directive('quickStatistics', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/quick-statistics.html',
+        }
+
+    });
