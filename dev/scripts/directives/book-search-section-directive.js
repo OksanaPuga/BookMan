86c9b10811bf3920049fe2d3b47a9e6e9cdab26e
Oksana_Puga <puga.oksana@gmail.com>
2015-08-16 03:40:04 +0300

    Added functionality for searching books in own library

---
+++ dev/scripts/directives/book-search-section-directive.js
@@ -0,0 +1,9 @@
+angular.module('bookman')
+    .directive('bookSearchSection', function () {
+
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/book-search-section.html',
+        }
+
+    });
