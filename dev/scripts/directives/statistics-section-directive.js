1c216bfd9034d50dc5d01f3b386199f614289e69
Oksana_Puga <puga.oksana@gmail.com>
2015-08-17 13:48:25 +0300

    Removed filter from book-list page and search-page, created individual controller for statistics-section (button and number of books)

--- dev/scripts/directives/statistics-section-directive.js
+++ dev/scripts/directives/statistics-section-directive.js
@@ -1,7 +1,11 @@
 angular.module('bookman')
-    .directive('statisticsSection', function () {
+    .directive('statisticsSection', ['Books', function (Books) {
         return {
             restrict: 'E',
-            templateUrl: 'templates/directives/statistics-section.html'
+            templateUrl: 'templates/directives/statistics-section.html',
+            controller: function () {
+                this.books = Books.getAllBooks();
+            },
+            controllerAs: 'statSecCtrl'
         };
-    });
+    }]);
